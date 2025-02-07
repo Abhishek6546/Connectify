ac4243752727c8dbcd8b502aafc1fe9ee7677ce0
Shruti-2304 <shrutikharb23@gmail.com>
2025-02-07 11:11:42 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,7 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+const __dirname = path.resolve();
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -33,7 +34,6 @@ if(process.env.NODE_ENV === "production"){
 };
 
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
